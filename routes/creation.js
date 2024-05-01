bec351dfb7b7216aeeffeda1e56d97df1cc4ea9b
Laura Valente <lauravalentelvp@gmail.com>
2024-05-01 15:36:49 -0300

    criacao da logica para criacao de uma nova pagina, template mustache para a nova pagina e criacao da modal de feedback ao criar

--- routes/creation.js
+++ routes/creation.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-// Rota GET para ir para a pagina de criacao de conteúdo /creation
+// rota GET para ir para a pagina de criacao de conteúdo
 router.get('/creation', (req, res) => {
-    res.render('creation')
-})
+    // verifica se há um parâmetro de sucesso e renderiza com o modal de feedback
+    if (req.query.success) {
+        res.render('creation', {
+            success: true,
+            url: `http://localhost:3000/${req.query.route}`
+        });
+    } else {
+        res.render('creation');
+    }
+});
 
-module.exports = router
+// rota POST para receber os dados do formulário e criar uma rota com o título e o conteúdo que o admin passou
+router.post('/creation', (req, res) => {
+    const { url, title, content } = req.body;
+    // registro dinâmico de nova rota
+    router.get(`/${url}`, (req, res) => {
+        res.render('template', { title, content });
+    });
+
+    // redireciona para /creation com parâmetro de sucesso
+    res.redirect(`/creation?success=true&route=${url}`);
+});
+
+module.exports = router;
