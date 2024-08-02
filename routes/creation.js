d84881893d4a72fd0559104a97f60268bafe7504
gabrielfurtadon <gabrielfurtado@alunos.utfpr.edu.br>
2024-08-02 14:52:25 -0300

    alterando pagina do admin e adicionando validacao de user logado à rotas

--- routes/creation.js
+++ routes/creation.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { isLogged } = require('../helpers/access');
 const router = express.Router();
 
 // rota GET para ir para a pagina de criacao de conteúdo
-router.get('/creation', (req, res) => {
+router.get('/creation', isLogged, (req, res) => {
     // verifica se há um parâmetro de sucesso e renderiza com o modal de feedback
     if (req.query.success) {
         res.render('creation', {
@@ -15,7 +16,7 @@ router.get('/creation', (req, res) => {
 });
 
 // rota POST para receber os dados do formulário e criar uma rota com o título e o conteúdo que o admin passou
-router.post('/creation', (req, res) => {
+router.post('/creation', isLogged, (req, res) => {
     const { url, title, content } = req.body;
     // registro dinâmico de nova rota
     router.get(`/${url}`, (req, res) => {
